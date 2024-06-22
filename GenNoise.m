3b5a37dd3c728a7ef114ae85c33e3abcd5c7f97b
Matteo Farè <32509505+Sabaudian@users.noreply.github.com>
2024-06-22 11:30:12 +0200

    Update GenNoise.m

--- GenNoise.m
+++ GenNoise.m
@@ -1,5 +1,5 @@
 function [noise] = GenNoise(N,L)
-%Generate randon Gaussian noise
+% Generate random Gaussian noise as a mixture model
 %   INPUT:
 %   - N: number of data in the dataset (row)
 %   - L: length of the signals (column)
@@ -8,31 +8,39 @@ function [noise] = GenNoise(N,L)
 %           between the interval [a b]
 
 % Prealloc data
-E = zeros(1,N); % 
-Sigma1 = zeros(1,N); % Standard deviation
-Sigma2 = zeros(1,N); % Standard deviation
-snr = zeros(1,N); % SNR
+E = zeros(1,N); % Epsilon (weight)
+Sigma1 = zeros(1,N); % Standard deviation of G1
+Sigma2 = zeros(1,N); % Standard deviation of G2
 noise = zeros(N,L); % output noise
+
 % Epsilon Range
 aE = 0.1; bE = 0.5;
-% As Sigma1 and Sigma2 increases, the noise amplitude increases
-% Standard Deviation Range
+
+% As Sigma1 and Sigma2 increase, the noise amplitude increases
+% Standard Deviation Range for Sigma1
 aS1 = 0.01; bS1 = 0.03;
-% SNR range
-aSnr = 0.5; bSnr = 1.5;
-% Generate random variable to gen. noise
+
+% Generate random variables to gen. noise
 for i = 1:N
-    E(i) = aE + (bE-aE).*rand(1);
-    Sigma1(i) = aS1 + (bS1-aS1).*rand(1);
+    E(i) = aE + (bE-aE).*rand(1); % Generate epsilon
+    Sigma1(i) = aS1 + (bS1-aS1).*rand(1); % Generate Sigma1
+    
+    % Standard Deviation Range for Sigma2
     aS2 = 2*Sigma1(i); bS2 = 20*Sigma1(i);
-    Sigma2(i) = aS2 + (bS2-aS2).*rand(1);
-    snr(i) = aSnr + (bSnr - aSnr).*rand(1);
+    Sigma2(i) = aS2 + (bS2-aS2).*rand(1); % Generate Sigma2
 end
+
 % Generate noise
 for i = 1:N
-    noise(i,:) = snr(i)*((1-E(i))*Sigma1(i).*randn(L,1) + E(i)*Sigma2(i).*randn(L,1));
+    % Generate Gaussian noise with standard deviation Sigma1
+    G1 = Sigma1(i) .* randn(L,1);
+    % Generate Gaussian noise with standard deviation Sigma2
+    G2 = Sigma2(i) .* randn(L,1);
+    % Combine them according to the mixture model
+    noise(i,:) = (1 - E(i)) * G1' + E(i) * G2';
 end
-% Sort the noise ascending order
-noise = sort(noise); 
+
+% Sort the noise in ascending order
+noise = sort(noise, 2); % Sort along each row
 
 end
